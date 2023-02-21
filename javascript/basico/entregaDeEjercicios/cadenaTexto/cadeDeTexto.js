467f294aea4ae18c04a630c5e54d256c4faf8059
pablinux87 <pabloratero@gmail.com>
2023-02-21 15:07:49 -0300

    Entrega ejercicio de cadena de texto

---
+++ javascript/basico/entregaDeEjercicios/cadenaTexto/cadeDeTexto.js
@@ -0,0 +1,34 @@
+/*
+Crea un archivo JS que contenga las siguientes líneas
+
+- Una cadena de texto con tu Nombre
+
+- Otra cadena de texto con tu Apellido
+
+- Una cadena de texto que se llame "estudiante" concatenando tu Nombre y tu Apellido con un espacio entre medias
+
+- Una cadena de texto que se llame "estudianteMayus" que contenga la cadena estudiante pero todo en mayúsculas
+
+- Una cadena de texto que se llame "estudianteMinus" que contenga la cadena estudiante pero todo en minúsculas
+
+- Una variable que contenga el número de letras de la cadena "estudiante" contando los espacios
+
+- Una variable que contenga la primera letra del Nombre
+
+- Otra variable que contenga la última letra del Apellido
+
+- Una cadena de texto que elimine los espacios de la variable "estudiante"
+
+- Una variable booleana que diga si el Nombre está contenido en la variable "estudiante"
+*/
+
+const nombre = "Pablo";
+const apellido = "Ratero";
+const estudiante = `${nombre} ${apellido}`;
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const cantidadLetras = estudiante.length;
+const inicialNombre = nombre.charAt(0);
+const finalApellido = apellido.slice(-1);
+const sinEspacios = estudiante.replace(/ /g, "");
+const boolNombre = estudiante.includes(nombre);
