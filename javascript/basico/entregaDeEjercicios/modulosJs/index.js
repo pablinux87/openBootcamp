0cf4fc2fb583dadc6cdec8e9498f3b5c8d0e52a2
pablinux87 <pabloratero@gmail.com>
2023-02-25 18:50:59 -0300

    Entrega ejercicio modulos

---
+++ javascript/basico/entregaDeEjercicios/modulosJs/index.js
@@ -0,0 +1,26 @@
+/**
+Crea un nuevo proyecto de Node
+
+- Configura el proyecto para utilizar los módulos de ES6
+
+- Crea un archivo controller.js que exporte 2 funciones: suma(a, b) y multiplica(a, b)
+
+- En el entrypoint index.js, importa el módulo controller.js
+
+- El entrypoint index.js debe utilizar las funciones del módulo para devolver la multiplicación de suma(1, 2) y suma(4, 5)
+
+- Instala e importa la librería chalk (https://www.npmjs.com/package/chalk)
+
+- Modifica el último console.log del entrypoint index.js para devolver el resultado en color verde
+ */
+import * as mat from "./modules/controller.js";
+import chalk from "chalk";
+
+const log = console.log;
+const multi = mat.multi(1, 2);
+const sum = mat.suma(4, 5);
+
+log(chalk.red.bold(multi));
+log(chalk.bgGreen.blueBright.bold(`        ${sum}        `));
+
+log(chalk.green("Ultimo clg color green!!"));
