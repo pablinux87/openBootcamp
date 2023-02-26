7d9b82f576feca9e2ae21fc61704a9b7ff2d5c59
pablinux87 <pabloratero@gmail.com>
2023-02-26 16:04:41 -0300

    Cambio de gitignore

--- javascript/basico/introduccion_a_Javascript/sesion-14-js-html/jquery/index.js
+++ javascript/basico/introduccion_a_Javascript/sesion-14-js-html/jquery/index.js
@@ -4,35 +4,34 @@
 
 // $(document).ready(() => {
 $(() => {
-    // Selectores:
-    // id="el-1" => "#el-1"
-    // class="el-1" => ".el-1"
-    // $("#el-1").hide()
+  // Selectores:
+  // id="el-1" => "#el-1"
+  // class="el-1" => ".el-1"
+  // $("#el-1").hide()
 
-    $(".hide-btn").click(() => {
-        // $("h1").hide()
-        $("h1").fadeOut()
-    })
-    $(".show-btn").click(() => {
-        // $("h1").show()
-        $("h1").fadeIn()
-    })
+  $(".hide-btn").click(() => {
+    // $("h1").hide()
+    $("h1").fadeOut();
+  });
+  $(".show-btn").click(() => {
+    // $("h1").show()
+    $("h1").fadeIn();
+  });
 
-    $("li").dblclick(() => {
-        $("h1").css({ color: "red" })
-    })
+  $("li").dblclick(() => {
+    $("h1").css({ color: "red" });
+  });
 
-    $(".new-element").click(() => {
-        // $("ul").append("<li>New Element</li>")
-        $("ul").prepend("<li>New Element</li>")
-    })
+  $(".new-element").click(() => {
+    // $("ul").append("<li>New Element</li>")
+    $("ul").prepend("<li>New Element</li>");
+  });
 
-    $("li").mouseenter((elem) => {
-        elem.target.style.color = "blue"
-    })
+  $("li").mouseenter((elem) => {
+    elem.target.style.color = "blue";
+  });
 
-    $("li").mouseleave(elem => {
-        elem.target.style.color = "black"
-    })
-
-})
+  $("li").mouseleave((elem) => {
+    elem.target.style.color = "black";
+  });
+});
