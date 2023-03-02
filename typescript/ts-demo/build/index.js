d02f2365352c6c6d113af7242f2d9a9a9151a109
pablinux87 <pabloratero@gmail.com>
2023-03-02 17:23:52 -0300

    Update typescript

--- typescript/ts-demo/build/index.js
+++ typescript/ts-demo/build/index.js
@@ -1,2 +1,9 @@
 "use strict";
 console.log("Prueba de archivo TS");
+var nombre = "Pablo";
+console.log("Hola, " + nombre);
+console.log("Que tal", nombre, "?");
+console.log(`Como estas ${nombre}`);
+let email = "pabloratero@gmail.com";
+console.log(`El email: ${email} pertenece a ${nombre}`);
+const PI = 3.14;
--- typescript/ts-demo/src/index.ts
+++ typescript/ts-demo/src/index.ts
@@ -1,1 +1,88 @@
+import { type } from "os";
+
 console.log("Prueba de archivo TS");
+
+var nombre: string = "Pablo";
+
+console.log("Hola, " + nombre);
+console.log("Que tal", nombre, "?");
+console.log(`Como estas ${nombre}`);
+
+let email = "pabloratero@gmail.com";
+console.log(`El email: ${email} pertenece a ${nombre}`);
+
+const PI: number = 3.14;
+
+let error: boolean = false;
+
+// Instanciar variables muntiples:
+
+let a: string, b: boolean, c: number;
+
+a = "Olivia loca";
+b = true;
+c = 9;
+
+// BuiltIn types: number, string, boolean, void, null, undefined
+
+// Tipos mas complejos
+
+let listaTarea: string[] = ["Tarea1", "Tarea2"];
+
+// Combinacion en listas:
+let valores: (string | number | boolean)[] = [false, "Pablo", 9];
+
+// Enumerados:
+enum Estados {
+  "Completado" = "C",
+  "Incompleto" = "I",
+  "Pendiente" = "P",
+}
+
+let estadoTarea: Estados = Estados.Completado; // C
+
+enum PuestoCarrera {
+  "Primero" = 1,
+  "Segundo",
+  "Tercero",
+}
+
+let puestoCarrera: PuestoCarrera = PuestoCarrera.Segundo; // 2
+
+//Interfaces:
+interface Tarea {
+  nombre: string;
+  estado: Estados;
+  urgencia: number;
+}
+
+// Podemos crear variables que sigan la interface:
+
+let tarea1: Tarea = {
+  nombre: "Tarea1",
+  estado: Estados.Pendiente,
+  urgencia: 10,
+};
+
+// Types de typescript
+
+type Producto = {
+  precio: number;
+  nombre: string;
+  anio: number;
+};
+
+let coche: Producto = {
+  nombre: "AUDI",
+  precio: 45000,
+  anio: 2009,
+};
+
+// ** Condicionales:
+
+// Operdor ternario
+
+console.log(
+  `el Coche adquirido es un ${coche.nombre} y devido al modelo es`,
+  coche.anio < 2010 ? `un poco viejito...` : `bastnte nuevito...`
+);
